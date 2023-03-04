dbb3a3005c651c14a4648222ec13562d30b8ad52
Александр Фокин <wegnagun@bk.ru>
2023-03-04 19:23:12 +0300

    working at parse command

--- mems_like/mems_library/management/commands/parse_mems.py
+++ mems_like/mems_library/management/commands/parse_mems.py
@@ -1,8 +1,24 @@
+import requests
 from django.core.management.base import BaseCommand
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROUP_ID = -45045130
+POSTS_COUNT = 20
+API_VERSION = 5.81
 
 
 class Command(BaseCommand):
     help = 'Загрузите все мемы в вашу базу (используя API ВКонтакте).'
 
     def handle(self, *args, **options):
-        pass
+        params = {
+            'owner_id': GROUP_ID,
+            'count': POSTS_COUNT,
+            'v': API_VERSION
+        }
+        api_response = requests.get(
+            'https://api.vk.com/method/wall.get', params=params
+        )
+        print(api_response)
--- mems_like/mems_like/settings.py
+++ mems_like/mems_like/settings.py
@@ -53,10 +53,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mems_like.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
