c7833031d154cc032ff7e9a26943f01c019513ca
Александр Фокин <wegnagun@bk.ru>
2023-03-06 20:54:45 +0300

    add representation of image in admin

--- mems_like/mems_library/admin.py
+++ mems_like/mems_library/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.utils.html import format_html
+
 from .models import Mem
 
 
 @admin.register(Mem)
 class MemAdmin(admin.ModelAdmin):
-    list_display = ('mem_id', 'text', 'pub_date', 'post_author', 'likes_count')
+    list_display = (
+        'mem_id', 'img', 'text', 'pub_date', 'post_author', 'likes_count'
+    )
+    save_on_top = True
+
+    @admin.display(description='Картинка мема')
+    def img(self, obj):
+        return format_html(
+            f'<img src={obj.image_url} '
+            f'alt="не загрузилась" style=" width: 200px;">'
+        )
 
 
 admin.site.unregister(Group)
